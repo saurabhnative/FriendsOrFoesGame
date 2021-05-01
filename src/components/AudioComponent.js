4026716b797bf2b0bd439d454573169a5fad59e9
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-05-01 12:47:49 +0530

    AudioComponentUpdated

--- src/components/AudioComponent.js
+++ src/components/AudioComponent.js
@@ -1,26 +1,32 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeUp, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 import "./AudioComponent.css";
 export default function AudioComponent() {
   const [audioPlayback, setAudioPlayback] = useState(false);
-  const audioRef = useRef(null);
+  const [audioElement, setAudioElement] = useState([]);
   function toggleAudio() {
     if (!audioPlayback) {
-      audioRef.current.play();
+      const audioElement = [];
+      audioElement.push(
+        <>
+          <audio
+            src="https://res.cloudinary.com/dk22rcdch/video/upload/v1619806254/FriendsOrFoes/trimmedintro_s6baaj.mov"
+            preload="metadata"
+            loop="true"
+            autoPlay="true"
+          />
+        </>
+      );
+      setAudioElement(audioElement);
     } else {
-      audioRef.current.pause();
+      setAudioElement([]);
     }
     setAudioPlayback(!audioPlayback);
   }
   return (
     <div className="audio-component-container mt-3 absolute mx-2 float-right">
-      <audio
-        src="https://res.cloudinary.com/dk22rcdch/video/upload/v1619806254/FriendsOrFoes/trimmedintro_s6baaj.mov"
-        ref={audioRef}
-        preload="metadata"
-        loop="true"
-      />
+      {audioElement}
       <div className="audio-component-icon" onClick={() => toggleAudio()}>
         {audioPlayback ? (
           <FontAwesomeIcon icon={faVolumeMute} />
