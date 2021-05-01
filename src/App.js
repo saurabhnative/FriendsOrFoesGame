3198b3266030a0ab445f72b9bd2ed27b689a1ff1
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-05-01 12:54:11 +0530

    AudioComponent Removed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import "tailwindcss/dist/tailwind.css";
 
 import IntroductionScreen from "./components/IntroductionScreen";
 import GameScreen from "./components/GameScreen";
-import AudioComponent from "./components/AudioComponent";
+// import AudioComponent from "./components/AudioComponent";
 function App() {
   return (
     <Router>
       <div className="flex flex-col">
-        <AudioComponent />
         <Switch>
           <Route path="/" exact={true}>
             <IntroductionScreen />
